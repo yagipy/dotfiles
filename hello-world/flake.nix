320b8bb62ec1c76d3b43f33633c3d917f8731ad2
yagipy <email@yagipy.me>
2026-09-23 03:15:25 +0900

    sum_1_2 outputを追加

--- hello-world/flake.nix
+++ hello-world/flake.nix
@@ -1,6 +1,12 @@
 {
-  # inputs = {};
-  outputs = _inputs: {
+  inputs = {
+    sub_flake.url = "path:./sub";
+  };
+  outputs = { sub_flake, ... }: {
     hello = "Hello, world!";
+    sum_1_2 = sub_flake.add_a_b 1 2;
   };
 }
+
+# nix eval .#hello
+# nix eval .#sum_1_2
